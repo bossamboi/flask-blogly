1cb7c679a6401a6498300cdd956ca2b9e91a494e
Maurice <maurice.baez1@gmail.com>
2022-03-30 15:38:54 -0700

    added new user form and functionality

--- app.py
+++ app.py
@@ -18,19 +18,45 @@ db.create_all()
 
 @app.get("/")
 def redirect_to_users():
+    """ Redirect to display users """
     return redirect("/users")
+
 
 @app.get("/users")
 def show_users():
-    # get users from db
+    """ Show all current users """
+
     users = User.query.all()
-    # loop through and display each as link
 
-    # add link to add-user form
-    return render_template('user_listing.html', users = users)
+    return render_template("user_listing.html", users=users)
 
-# @app.get("/users/new")
-# @app.post("/users/new")
+
+@app.get("/users/new")
+def show_new_user_form():
+    """ Display new user form """
+
+    return render_template("new_user_form.html")
+
+
+@app.post("/users/new")
+def process_and_add_new_user():
+    """ Process form and add new user to database """
+
+    first_name = request.form.get("first-name")
+    last_name = request.form.get("last-name")
+    image_url = request.form.get("image-url")
+
+    new_user = User(first_name = first_name, last_name = last_name, image_url = image_url)
+
+# Add new object to session, so they'll persist
+    db.session.add(new_user)
+
+# Commit--otherwise, this never gets saved!
+    db.session.commit()
+
+    return redirect("/users")
+
+
 # @app.get("/users/<int:user_id>")
 # @app.get("/users/<int:user_id>/edit")
 # @app.post("/users/<int:user_id>/edit")
